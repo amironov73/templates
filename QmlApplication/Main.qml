741dcd4bc4f72d9d94ad05589ca698c72e071c31
Alexey Mironov <amironov73@gmail.com>
2021-05-11 13:12:57 +0800

    Add QML application

---
+++ QmlApplication/Main.qml
@@ -0,0 +1,67 @@
+﻿import QtQuick 2.7
+import QtQuick.Controls 2.0
+import QtQuick.Layouts 1.15
+import QtQuick.Dialogs 1.3
+
+import app 1.0
+ 
+ApplicationWindow {
+    id: applicationWindow
+    visible: true
+    width: 340
+    height: 280
+    title: "Hello World"
+    
+    NetObject {
+        id: model
+    }
+    
+    MessageDialog {
+        id: messageDialog
+        icon: StandardIcon.Question
+        title: "May I have your attention please"
+        text: "It's so cool that you are using Qt Quick."
+        onAccepted: {
+            console.log("And of course you could only agree.")
+            Qt.quit()
+        }
+    }
+    
+    ColumnLayout {
+        anchors.fill: parent
+        spacing: 6
+        
+        Text {
+            Layout.fillWidth: true
+            padding: 6
+            wrapMode: Text.Wrap
+            antialiasing: true 
+            text: "У попа была собака, он её любил. Она съела кусок мяса, он её убил. В землю закопал и надпись написал."
+            font.pixelSize: 14
+        }
+        
+        Text {
+            Layout.fillWidth: true
+            padding: 6
+            wrapMode: Text.Wrap
+            horizontalAlignment: Text.Center
+            antialiasing: true 
+            text: model.someUsefulText
+            font.pixelSize: 14
+            color: "red"
+        }
+    
+        Button {
+            Layout.fillWidth: true
+            text: "Нажми меня!"
+            onClicked: model.handleClick()
+        }
+ 
+        Button {
+            Layout.fillWidth: true
+            text: "И меня!"
+            onClicked: messageDialog.open()
+        }
+ 
+    }
+}
---
+++ QmlApplication/Program.cs
@@ -0,0 +1,86 @@
+﻿// ReSharper disable ClassNeverInstantiated.Global
+// ReSharper disable UnusedMember.Global
+
+using System;
+using System.IO;
+
+using Qml.Net;
+using Qml.Net.Runtimes;
+
+namespace HelloQml
+{
+    public class NetObject
+    {
+        public string SomeUsefulText { get; set; }
+            = "В чащах Юга жил-был цитрус, но фальшивый экземпляр!";
+
+        public void HandleClick()
+        {
+            Console.WriteLine("Нас нажали!");
+        }
+    }
+    
+    internal static class Program
+    {
+        private static string RuntimeTargetToString(RuntimeTarget target)
+        {
+            switch (target)
+            {
+                case RuntimeTarget.LinuxX64:
+                    return "linux-x64";
+                
+                case RuntimeTarget.OSX64:
+                    return "osx-x64";
+                
+                case RuntimeTarget.Windows64:
+                    return "win-x64";
+                
+                case RuntimeTarget.Unsupported:
+                    throw new Exception("Unsupported target");
+                
+                default:
+                    throw new Exception($"Unknown target {target}");
+            }
+        }
+
+        static void PrepareRuntime()
+        {
+            var qtVersion = QmlNetConfig.QtBuildVersion;
+            var runtimeTarget = RuntimeManager.GetCurrentRuntimeTarget();
+            var runtimeVersion = qtVersion + "-" + RuntimeTargetToString(runtimeTarget);
+            var runtimePath = Path.Combine
+                (
+                    AppContext.BaseDirectory,
+                    runtimeVersion
+                );
+
+            if (!Directory.Exists(runtimePath))
+            {
+                Directory.CreateDirectory(runtimePath);
+                RuntimeManager.DownloadRuntimeToDirectory
+                    (
+                        qtVersion, 
+                        runtimeTarget, 
+                        runtimePath
+                    );
+            }
+            
+            RuntimeManager.ConfigureRuntimeDirectory (runtimePath);
+        }
+        
+        [STAThread]
+        static int Main(string[] args)
+        {
+            PrepareRuntime();
+            
+            using var app = new QGuiApplication(args);
+            using var engine = new QQmlApplicationEngine();
+
+            Qml.Net.Qml.RegisterType<NetObject>("app");
+            
+            engine.Load("Main.qml");
+            
+            return app.Exec();
+        }
+    }
+}
